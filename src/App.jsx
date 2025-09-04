cbd36505460f5f4516027d8164eda741b48d5213
ManamsKay <manamelakamogelo71@gmail.com>
2025-09-04 20:20:51 +0200

    Added chatbot UI, now working on productList

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import ChatBot from "./components/chatBot"; // ✅ Import ChatBot component
+import ProductList from "./components/ProductList";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         {/* Additional content */}
       </main>
       <ChatBot /> {/* ✅ Chatbot appears bottom-right */}
+      <ProductList /> {/* ✅ Product list appears below main content */}
     </div>
   );
 }
